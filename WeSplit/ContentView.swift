55ffe274eed0b48cf8364458c90ca3ffbac4c336
AllanRaf <rafale.uk@googlemail.com>
2020-06-14 19:43:31 +0200

    feat: my first form.

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -10,17 +10,33 @@ import SwiftUI
 
 
 struct ContentView: View {
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 2
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
     var body: some View {
-        
         NavigationView{
-            
-            Form{
+            Form {
                 Section{
-                Text("Hello, World!")
+                    TextField("Amount", text: $checkAmount)
+                        .keyboardType(.decimalPad)
+                    
+                    Picker("Number of People", selection:$numberOfPeople ){
+                        ForEach(2..<100){
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                Section{
+                    Text("$\(checkAmount)")
                 }
             }
-            .navigationBarTitle("SwiftUI")
+             .navigationBarTitle("WeSplit")
         }
+   
+        
     }
 }
 struct ContentView_Previews: PreviewProvider {
