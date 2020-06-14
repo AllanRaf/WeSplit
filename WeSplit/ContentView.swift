90e1a20d5ada818978b6a7b373e606867c25c778
AllanRaf <rafale.uk@googlemail.com>
2020-06-14 22:15:56 +0200

    feature: add functionality to calculate amount owed.

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -11,10 +11,21 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var checkAmount = ""
-    @State private var numberOfPeople = 2
+    @State private var numberOfPeople = 0
     @State private var tipPercentage = 2
     
     let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var totalPerPerson:Double {
+        let peopleCount = Double(numberOfPeople+2)
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        let orderAmount = Double(checkAmount) ?? 0
+        
+        let tipValue = orderAmount*tipSelection/100
+        let grandTotal = orderAmount + tipValue
+        let amountPerPerson = grandTotal/peopleCount
+        return amountPerPerson
+    }
     
     var body: some View {
         NavigationView{
@@ -29,8 +40,16 @@ struct ContentView: View {
                         }
                     }
                 }
+                Section(header: Text("How much tip do you want to leave?")){
+                    Picker("Tip percentage", selection:$tipPercentage){
+                        ForEach(0..<tipPercentages.count){
+                            Text("\(self.tipPercentages[$0])%")
+                        }
+                    }
+                .pickerStyle(SegmentedPickerStyle())
+                }
                 Section{
-                    Text("$\(checkAmount)")
+                    Text("$\(totalPerPerson, specifier: "%.2f")" )
                 }
             }
              .navigationBarTitle("WeSplit")
