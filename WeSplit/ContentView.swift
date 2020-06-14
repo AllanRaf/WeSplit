1ff2ffd8172e052eb238f81731144318ca147217
AllanRaf <rafale.uk@googlemail.com>
2020-06-14 18:14:09 +0200

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Allan Man on 14/06/2020.
+//  Copyright © 2020 Allan Man. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
